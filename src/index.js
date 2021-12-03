f66959377fe908bd97db95814bea3447b6d62707
Marta Silva <martaangelsilva@hotmail.com>
2021-12-03 20:33:53 -0300

    atualizacao

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 //principio básico - acessar documentação http://expressjs.com/pt-br/starter/hello-world.html
 const express = require('express')
+
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.resolve() + '/src/views' ))
+
+
 app.get('/',(req,res) =>{
-    res.send('Olá mundo')
+    res.sendFile(path.resolve() + '/views/index.html')
 })
 
 app.listen(port, ()=>{console.log(`Example app listening at http://localhost:${port}`)})
+
